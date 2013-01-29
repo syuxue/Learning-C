2f03abe7e2729ca7e0f594e76a8fa5ec0872bb3f
Yuchen Wang <phobosw@gmail.com>
2013-01-29 16:52:32 +0800

    changes:convert file encode

--- 201212_c_programming_language/section_2.12_precedence_and_order.c
+++ 201212_c_programming_language/section_2.12_precedence_and_order.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /*
-����undefined behavior
+关于undefined behavior
 http://www.linuxsir.org/bbs/thread239136.html
 
-����¥������û�����⣬������ϸ������������Ƕ�Ӣ��ժ���������Ҫ������������еĸ���������Ԫ�ص������Ⱥ�˳�򣬶����ǹ�ϵ��prefix increment�Ķ����� 
+看来楼主还是没有理解，不妨仔细看看我上面的那段英文摘抄。这儿主要的问题是语句中的各个单独的元素的运算先后顺序，而不是关系到prefix increment的东西： 
 r = (++i) + (++i); 
-��������ǰ������++i�ĵ���˳���Լ�ȡi��˳���ǲ�ȷ���ġ�����undefined behavior��ȡ���ڱ���������������ȡ��i��ֵ�������2����Ȼ��ֱ�������++i������ӣ����ǽ�����ǣ� 
+这个语句中前后两个++i的调用顺序以及取i的顺序是不确定的。就是undefined behavior，取决于编译器，它可以先取得i的值（这儿是2），然后分别做两次++i，再相加，于是结果就是： 
 r = 6 
-Ȼ��������Ҳ�����ȼ����һ��++i�����ܵڶ����������Ȱ�i������Ȼ���ֵ���i��Ȼ����ִ�еڶ���++i��Ȼ��ֱ�ӽ����εĽ��tmpֵ�������Ľ�������ǣ� 
+然而编译器也可以先计算第一个++i而不管第二个，就是先把i递增，然后把值存回i，然后再执行第二个++i，然后直接将两次的结果tmp值相加这儿的结果将会是： 
 r = 7 
-�����ǣ������ȡi�ĵ�ַ����ֵ��ӣ����õ��� 
+或者是，最后还是取i的地址处的值相加，而得到： 
 r = 8 
-��Щ˳��C�ı�׼��û�й涨�ģ���ȡ���ڱ���������Ϊ���Ӳ�������Լ��Ż�֮���������ء� 
+这些顺序C的标准是没有规定的，这取决于编译器，因为这和硬件构架以及优化之类的密切相关。 
 
 The moral is that writing code that depends on order of evaluation   
 is a bad programming practice in any language. Naturally, it is  
