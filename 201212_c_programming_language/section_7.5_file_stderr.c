aacb6b973f267ec778b299cdf919b382c26eb7c7
Yuchen Wang <phobosw@gmail.com>
2013-02-03 23:58:06 +0800

    improved:顺手完成习题 Exercise 7-8. Write a program to print a set of files, starting each new one on a new page,with a title and a running page count for each file.

--- 201212_c_programming_language/section_7.5_file_stderr.c
+++ 201212_c_programming_language/section_7.5_file_stderr.c
@@ -5,22 +5,28 @@
 // Main
 int main(int argc, char *argv[])
 {
-	int i, c, filenumber;
+	int i, c, filenum, linenum;
 	char *filelist[argc - 1];
 	FILE *fp;
 
 	// 获取文件名列表
-	filenumber = argc - 1;
-	for (i = 0; i < filenumber; i++) {
+	filenum = argc - 1;
+	for (i = 0; i < filenum; i++) {
 		filelist[i] = argv[i + 1];
 	}
 
 	// 输出文件内容
-	for (i = 0; i < filenumber; i++) {
+	for (i = 0; i < filenum; i++) {
 		if ((fp = fopen(filelist[i], "r")) != NULL) {
-			M_print("%s", filelist[i]);
-			while ((c = getc(fp)) != EOF)
+			printf(M_bash_magenta "********** %d/%d %s **********\n" M_bash_default, i + 1, filenum, filelist[i]);
+			linenum = 0;
+			printf(M_bash_green "%-4d" M_bash_cyan ": " M_bash_default, ++linenum);
+			while ((c = getc(fp)) != EOF) {
 				putchar(c);
+				if (c == '\n')
+					printf(M_bash_green "%-4d" M_bash_cyan ": " M_bash_default, ++linenum);
+			}
+			putchar('\n');
 			fclose(fp);
 		} else {
 			fprintf(stderr, "%s: can't open %s\n", argv[0], filelist[i]);
