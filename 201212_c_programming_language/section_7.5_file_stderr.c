994de34f6af0ed18a23c6deec652860589b93440
Yuchen Wang <phobosw@gmail.com>
2013-02-05 15:42:44 +0800

    improved:for exercise 8.1

--- 201212_c_programming_language/section_7.5_file_stderr.c
+++ 201212_c_programming_language/section_7.5_file_stderr.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/m_function.h"
+//#define BENCH
 
-// Main
+// Use getc, fopen, fclose in stdio.h write the program Cat
 int main(int argc, char *argv[])
 {
 	int i, c, filenum, linenum;
@@ -17,21 +18,28 @@ int main(int argc, char *argv[])
 
 	// 输出文件内容
 	for (i = 0; i < filenum; i++) {
-		if ((fp = fopen(filelist[i], "r")) != NULL) {
-			printf(M_bash_magenta "********** %d/%d %s **********\n" M_bash_default, i + 1, filenum, filelist[i]);
-			linenum = 0;
-			printf(M_bash_green "%-4d" M_bash_cyan ": " M_bash_default, ++linenum);
-			while ((c = getc(fp)) != EOF) {
-				putchar(c);
-				if (c == '\n')
-					printf(M_bash_green "%-4d" M_bash_cyan ": " M_bash_default, ++linenum);
-			}
-			putchar('\n');
-			fclose(fp);
-		} else {
+		if ((fp = fopen(filelist[i], "r")) == NULL) {
 			fprintf(stderr, "%s: can't open %s\n", argv[0], filelist[i]);
 			exit(1);
 		}
+
+#ifdef BENCH
+		while ((c = getc(fp)) != EOF)
+			;
+#else
+		linenum = 0;
+		printf(M_bash_magenta "********** %d/%d %s **********\n" M_bash_default, i + 1, filenum, filelist[i]);
+		printf(M_bash_green "%-4d" M_bash_cyan ": " M_bash_default, ++linenum);
+		while ((c = getc(fp)) != EOF) {
+			putchar(c);
+			if (c == '\n')
+				printf(M_bash_green "%-4d" M_bash_cyan ": " M_bash_default, ++linenum);
+		}
+		putchar('\n');
+#endif
+		
+		fclose(fp);
+
 		if (ferror(stdout)) {
 			fprintf(stderr, "%s: error writing stdout\n", argv[0]);
 			exit(2);
